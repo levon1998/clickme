41ac2f86e79b5d200310c0829a16a4a03ec6876b
kristal111 <levon.babayan_2013@mail.ru>
2017-11-05 23:31:28 +0400

    80%

--- database/migrations/2017_11_05_085319_create_score_table.php
+++ database/migrations/2017_11_05_085319_create_score_table.php
@@ -15,7 +15,7 @@ class CreateScoreTable extends Migration
     {
         Schema::create('score', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('score');
+            $table->string('score')->default(0);
             $table->integer('user_id')->unsigned();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
